2693193064cce47e0771c8c5043794b6b7d61f12
kelenna favour oonyeachonam <fkelenna@gmail.com>
2024-05-28 03:36:21 +0100

    book store front-end

---
+++ src/main/java/com/book_frontend/BookFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.book_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFrontendApplication.class, args);
+	}
+
+}
